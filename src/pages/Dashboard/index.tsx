dfe60b27e85ac60c6ea6b3ef1304f92ee6b10a22
Bruno Barbosa <barbosa.developer@gmail.com>
2020-04-20 21:21:11 -0300

    Estilizando Detalhe e Listando Issues

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -79,7 +79,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <Link key={repository.full_name} to={`${repository.full_name}`}>
+          <Link
+            key={repository.full_name}
+            to={`repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
